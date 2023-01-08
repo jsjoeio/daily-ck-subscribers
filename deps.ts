85e24f1911f30da2e6d7ff92dbae6e5bb5c6a0c5
Joe Previte <jjprevite@gmail.com>
2023-01-08 05:15:19 +0000

    formatting

--- deps.ts
+++ deps.ts
@@ -1,11 +1,11 @@
-import { config as dotEnvConfig } from 'https://deno.land/x/dotenv@v1.0.1/mod.ts';
+import { config as dotEnvConfig } from "https://deno.land/x/dotenv@v1.0.1/mod.ts";
 
-const {CK_API_SECRET_KEY, TELEGRAM_API_BOT_TOKEN} = dotEnvConfig({})
+const { CK_API_SECRET_KEY, TELEGRAM_API_BOT_TOKEN } = dotEnvConfig({});
 const apiBaseUrl = "https://api.convertkit.com/v3";
 
 async function getTotalSubscribers(): Promise<number> {
   const res = await fetch(
-    `${apiBaseUrl}/subscribers?api_secret=${CK_API_SECRET_KEY}`
+    `${apiBaseUrl}/subscribers?api_secret=${CK_API_SECRET_KEY}`,
   );
   const data = await res.json();
   return data.total_subscribers;
@@ -14,17 +14,20 @@ async function getTotalSubscribers(): Promise<number> {
 const telegramApiBaseUrl = "https://api.telegram.org/bot";
 
 async function sendMessage(text: string): Promise<void> {
-  const res = await fetch(`${telegramApiBaseUrl}${TELEGRAM_API_BOT_TOKEN}/sendMessage`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      chat_id: "751913803",
-      text: text,
-    }),
-  });
+  const res = await fetch(
+    `${telegramApiBaseUrl}${TELEGRAM_API_BOT_TOKEN}/sendMessage`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        chat_id: "751913803",
+        text: text,
+      }),
+    },
+  );
   if (res.status !== 200) {
     throw new Error(`Failed to send message: ${await res.text()}`);
   }
 }
- 
+
 export { dotEnvConfig, getTotalSubscribers, sendMessage };
--- index.ts
+++ index.ts
@@ -1,5 +1,8 @@
-import { getTotalSubscribers, sendMessage } from './deps.ts';
+import { getTotalSubscribers, sendMessage } from "./deps.ts";
 
 const typescriptCourseTotalSubscribers = await getTotalSubscribers();
-await sendMessage(`TypeScript Course subscribers: ${typescriptCourseTotalSubscribers.toLocaleString('en-US')}`);
-
+await sendMessage(
+  `TypeScript Course subscribers: ${
+    typescriptCourseTotalSubscribers.toLocaleString("en-US")
+  }`,
+);
