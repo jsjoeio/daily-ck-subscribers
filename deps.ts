7f1b8dc0a566d07a1c95d7297a385c917e7c75f4
Joe Previte <jjprevite@gmail.com>
2023-01-08 05:37:24 +0000

    refactor: adjust env vars

--- deps.ts
+++ deps.ts
@@ -1,13 +1,24 @@
 import { config as dotEnvConfig } from "https://deno.land/x/dotenv@v1.0.1/mod.ts";
 
-const { CK_API_SECRET_KEY, TELEGRAM_API_BOT_TOKEN } = dotEnvConfig({});
+function getEnvVar(key: string) {
+  if (Deno.env.get("GITHUB_ACTIONS") === "true") {
+    return Deno.env.get(key);
+  } else {
+    console.log("getting env vars from .env");
+    const envVars = dotEnvConfig({});
+    return envVars[key];
+  }
+}
+
+const CK_API_SECRET_KEY = getEnvVar("CK_API_SECRET_KEY");
+const TELEGRAM_API_BOT_TOKEN = getEnvVar("TELEGRAM_API_BOT_TOKEN");
+
 const apiBaseUrl = "https://api.convertkit.com/v3";
 
 async function getTotalSubscribers(): Promise<number> {
   const res = await fetch(
     `${apiBaseUrl}/subscribers?api_secret=${CK_API_SECRET_KEY}`,
   );
-  console.log(res, "response ");
   const data = await res.json();
   return data.total_subscribers;
 }
